47a64a26104ab070061cefc1bb053b06b2e629be
Brett Maden <brett.m.maden@omb.eop.gov>
2021-07-15 07:52:01 -0400

    return pojo from getQuestions

--- backend/src/main/java/gov/cdc/usds/simplereport/service/idverification/ExperianService.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/service/idverification/ExperianService.java
@@ -1,14 +1,15 @@
 package gov.cdc.usds.simplereport.service.idverification;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import gov.cdc.usds.simplereport.api.model.accountrequest.IdentityVerificationAnswersRequest;
 import gov.cdc.usds.simplereport.api.model.accountrequest.IdentityVerificationAnswersResponse;
-import gov.cdc.usds.simplereport.api.model.accountrequest.IdentityVerificationRequest;
+import gov.cdc.usds.simplereport.api.model.accountrequest.IdentityVerificationQuestionsRequest;
+import gov.cdc.usds.simplereport.api.model.accountrequest.IdentityVerificationQuestionsResponse;
 
 public interface ExperianService {
 
   /** Retrieves questions from Experian, given user data. */
-  public JsonNode getQuestions(IdentityVerificationRequest userData);
+  public IdentityVerificationQuestionsResponse getQuestions(
+      IdentityVerificationQuestionsRequest userData);
 
   public IdentityVerificationAnswersResponse submitAnswers(
       IdentityVerificationAnswersRequest answerRequest);
